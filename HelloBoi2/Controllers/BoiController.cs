9f3e581287f52a2ee7a80cc7c7227462a44a4d4a
Sai_Dinesh <saidinesh@texsync.com>
2022-03-16 11:15:41 +0530

    initial commit

---
+++ HelloBoi2/Controllers/BoiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloBoi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoiController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok("Hello BOI");
+        }
+    }
+}
